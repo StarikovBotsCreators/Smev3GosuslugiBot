120498db5e7fb530f852ccfa3bc34882ab70104c
wallacce135 wallacce135 <wallacce135@gmail.com>
2022-04-03 21:29:59 +0300

    Half parser done

--- Parser/IParser.cs
+++ Parser/IParser.cs
@@ -5,6 +5,7 @@ namespace Parser
 {
     public class IParser
     {
+        Task Process();
         Task<List<Model>> GetByName(string namePart);
         Task<List<Model>> GetByParameters(ParserSettings parserSettings);
     }
