3c7ee18d686d41b138d8f9358e1c428430a2661b
wallacce135 wallacce135 <wallacce135@gmail.com>
2022-04-04 12:29:21 +0300

    Regional & Federal news(full) done

--- ParsingBackground/Parsing.cs
+++ ParsingBackground/Parsing.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +12,9 @@ namespace ParsingBackground
 {
     internal class Parsing
     {
-        ParsedData set;
-        //Parsing(DataSet _set)
-        //{
-        //    set = _set;
-        //}
-
+        /// <summary>
+        /// Метод запускающий бразуер и Chome Driver
+        /// </summary>
         public void startBrowser()
         {
             var driver = new ChromeDriver(AppDomain.CurrentDomain.BaseDirectory + "\\parserFiles\\chromeFiles");
@@ -24,16 +22,24 @@ namespace ParsingBackground
             Searching(driver);
         }
 
+        /// <summary>
+        /// Метод, который является основной частью парсера
+        /// </summary>
+        /// <param name="driver"></param>
         public void Searching(ChromeDriver driver)
         {
-            for (int k = 0; k < 5; k++)
+
+            driver.Url = "https://smev3.gosuslugi.ru/portal/inquirytype.jsp?zone=fed";
+            System.Threading.Thread.Sleep(5000);
+
+            IWebElement max = driver.FindElement(By.XPath("//*[@id='news_0']/ul/li[12]/a"));
+            int maximum = Convert.ToInt32(max.Text);
+
+
+            for (int k = 0; k < maximum; k++)
             {
 
-
-                driver.Url = "https://smev3.gosuslugi.ru/portal/inquirytype.jsp?zone=fed";
-                System.Threading.Thread.Sleep(5000);
-
-                IWebElement rightArrow = driver.FindElement(By.XPath("//*[@id='news_0']/ul/li[13]/a"));
+                IWebElement rightArrow = driver.FindElement(By.ClassName("fa-angle-right"));
 
                 var nameTable = driver.FindElements(By.ClassName("cellOwner"));
 
@@ -69,13 +75,62 @@ namespace ParsingBackground
                     });
                 }
 
-                for (int i = 0; i < resultData.Count; i++)
+                rightArrow = driver.FindElement(By.ClassName("fa-angle-right"));
+                rightArrow.Click();
+                System.Threading.Thread.Sleep(3000);
+            }
+
+
+
+
+            driver.Navigate().GoToUrl("https://smev3.gosuslugi.ru/portal/inquirytype.jsp?zone=reg");
+            System.Threading.Thread.Sleep(5000);
+
+            IWebElement maxReg = driver.FindElement(By.XPath("//*[@id='news_0']/ul/li[12]/a"));
+            int maximumReg = Convert.ToInt32(maxReg.Text);
+            Console.WriteLine(maximumReg);
+
+            for (int k = 0; k < maximumReg; k++)
+            {
+
+                IWebElement rightArrow = driver.FindElement(By.ClassName("fa-angle-right"));
+
+                var nameTableReg = driver.FindElements(By.ClassName("cellOwner"));
+
+                var targetTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[2]"));
+
+
+                var identifierTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[3]"));
+
+                var scopeOfDataTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[4]"));
+
+                var versionTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[5]"));
+
+                var mpVersionTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[6]"));
+
+                var registrationDateTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[7]"));
+
+                var linkTableReg = driver.FindElements(By.XPath("/html/body/div[1]/div/div/div/div/div/div/table/tbody/tr/td[1]/a"));
+
+                List<ParsedData> resultDataReg = new List<ParsedData>();
+
+                for (int i = 0; i < nameTableReg.Count; i++)
                 {
-                    Console.WriteLine(resultData[i].Name);
-                    Console.WriteLine(resultData[i].Link);
+
+                    resultDataReg.Add(new ParsedData
+                    {
+                        Name = nameTableReg[i].Text,
+                        Target = targetTableReg[i].Text,
+                        Identifier = identifierTableReg[i].Text,
+                        ScopeOfData = scopeOfDataTableReg[i].Text,
+                        Version = versionTableReg[i].Text,
+                        MPVersion = mpVersionTableReg[i].Text,
+                        RegistrationDate = registrationDateTableReg[i].Text,
+                        Link = linkTableReg[i].GetAttribute("href"),
+                    });
                 }
 
-                rightArrow = driver.FindElement(By.XPath("//*[@id='news_0']/ul/li[13]/a"));
+                rightArrow = driver.FindElement(By.ClassName("fa-angle-right"));
                 rightArrow.Click();
                 System.Threading.Thread.Sleep(3000);
             }
